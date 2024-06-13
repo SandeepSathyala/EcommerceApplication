309ac1c2e33cf45fe018c72b9398bea361b4a333
SandeepSathyala <sandybalu001@gmail.com>
2024-06-13 15:13:33 +0530

    navbar and footer completed

--- my-ecommerce-app/src/app/app.component.ts
+++ my-ecommerce-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from './components/Core Components/header-component/header-component.component';
+import { FooterComponentComponent } from './components/Core Components/footer-component/footer-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponentComponent,FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
