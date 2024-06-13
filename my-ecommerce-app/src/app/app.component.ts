5896ebf0f54f062fa9f92321f4f19e4fc21dfecd
SandeepSathyala <sandybalu001@gmail.com>
2024-06-13 16:56:49 +0530

    signup page created

--- my-ecommerce-app/src/app/app.component.ts
+++ my-ecommerce-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from './components/Core Components/header-component/header-component.component';
 import { FooterComponentComponent } from './components/Core Components/footer-component/footer-component.component';
+import { SignupComponent } from './components/Authentication Components/signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponentComponent,FooterComponentComponent],
+  imports: [RouterOutlet,HeaderComponentComponent,FooterComponentComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
