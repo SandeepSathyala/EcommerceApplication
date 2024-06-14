40ea29d310ff9be10bbb54952d2aebfd3ce102f7
SandeepSathyala <sandybalu001@gmail.com>
2024-06-14 17:06:03 +0530

    database properties added

---
+++ EcommerceApplication/src/main/java/com/example/EcommerceApplication/dto/UserDto.java
@@ -0,0 +1,86 @@
+package com.example.EcommerceApplication.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+	    private int userId;
+	    private String username;
+	    private String email;
+	    private String firstName;
+	    private String lastName;
+	    private long phone;
+	    private String address;
+	    private LocalDateTime createdAt;
+	    private LocalDateTime updatedAt;
+	    private String role;
+		public int getUserId() {
+			return userId;
+		}
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public long getPhone() {
+			return phone;
+		}
+		public void setPhone(long phone) {
+			this.phone = phone;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public LocalDateTime getCreatedAt() {
+			return createdAt;
+		}
+		public void setCreatedAt(LocalDateTime createdAt) {
+			this.createdAt = createdAt;
+		}
+		public LocalDateTime getUpdatedAt() {
+			return updatedAt;
+		}
+		public void setUpdatedAt(LocalDateTime updatedAt) {
+			this.updatedAt = updatedAt;
+		}
+		public String getRole() {
+			return role;
+		}
+		public void setRole(String role) {
+			this.role = role;
+		}
+	    
+	    
+}
